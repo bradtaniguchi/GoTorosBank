c75372e775457f89e59818a2a2bcf35de9ca35ef
Brad <bradtaniguchi@gmail.com>
2016-09-20 15:45:30 -0700

    Added basic 1 user checking, no database

--- src/main/java/edu/csudh/goTorosBank/LoginManager.java
+++ src/main/java/edu/csudh/goTorosBank/LoginManager.java
@@ -4,21 +4,35 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
+/**
+ * Login Manager - manages user logins. Currently only checks a temp account
+ * TODO: Add a return cookie, to provide access to other parts of the application
+ * @author Bradley Taniguchi
+ * @see HttpServlet
+ */
 public class LoginManager extends HttpServlet {
-    String userName;
-    String passWord;
+    private String userName;
+    private String password;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         getServletContext().log("Init() called");
+        /*TODO: Initialize our 1 temp user ...*/
+        userName = "toro";
+        password = "gotoro";
     }
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         getServletContext().log("Service() called");
-        getServletContext().log("Brad was here!"); //test to see if I can find logs!
-        response.getWriter().write("This is the Reponse");
+        /*TODO: change this code, to do some actual legit checking...*/
+        if(request.getParameter("userName").equals(userName)) {
+            response.getWriter().write("Correct Login!");
+        }
+        else {
+            response.getWriter().write("Incorrect Login! Try again");
+        }
     }
 
     @Override
