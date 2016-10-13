d91ee87b13752b4caac74f21aba9359f119d5e3a
Brad <bradtaniguchi@gmail.com>
2016-10-12 19:23:45 -0700

    Created DatabaseInterface class
    Class is empty, but holds some comments about how to access the database.

---
+++ src/main/java/edu/csudh/goTorosBank/DatabaseInterface.java
@@ -0,0 +1,19 @@
+package edu.csudh.goTorosBank;
+
+import javax.sql.*;
+
+public class DatabaseInterface {
+    /*
+    * This code will give you access to the database
+    * I found that the Resource folder will be moved during deployment,
+    * the database will then appear in the following director:
+    * ROOT/WEB-INF/classes/GoTorosBank.db
+    * This can be accessed with the lines below.
+    *   -Brad
+            Connection c = null;
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite::resource:GoTorosBank.db" );
+     */
+
+
+}
