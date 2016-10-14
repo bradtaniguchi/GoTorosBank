3c0e79bf13573e13ef5f15b05f83992fde3455e3
rudyleiva93 <rudyleiva93@yahoo.com>
2016-10-13 23:56:56 -0700

    Added transfer class

---
+++ src/main/java/edu/csudh/goTorosBank/Trasnfer.java
@@ -0,0 +1,12 @@
+package edu.csudh.goTorosBank;
+
+/**
+ *
+ * @author Rudy
+ * 
+ * A transfer class which will handle transfers between
+ * Savings/Checkings accounts for the user 
+ */
+public class Trasnfer {
+    
+}
