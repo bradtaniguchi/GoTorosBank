ff6814b644354d93ae6831f57a37a0a82b2c0742
Brad <bradtaniguchi@gmail.com>
2016-10-14 13:07:08 -0700

    Added Functions to make comments to databaseInterface

--- src/main/java/edu/csudh/goTorosBank/DatabaseInterface.java
+++ src/main/java/edu/csudh/goTorosBank/DatabaseInterface.java
@@ -18,11 +18,20 @@ import java.sql.*;
 public class DatabaseInterface {
     public DatabaseInterface() throws Exception { //specify Exception
         Connection c = null;
-        //Statement stmt = null; // use this later
 
         Class.forName("org.sqlite.JDBC");
         c = DriverManager.getConnection("jdbc:sqlite::resource:GoTorosBank.db"); //this will get the file in resources
         /*do other stuff here*/
     }
+    // Functions to make:
+    //public validateUser(String user, String pass) - Crosby
+    //public getUser(String username) - brad
+    //  public payBill(int billID) -
+    //  public transfer(int accountIDFrom, int accountIDTo) -
+    //  public withdraw(int accountID, float amount) -
+    //  public deposit(int accountID, float amount) -
+    //private getAccounts(int userID) - Rudy
+    //private getTransactions(int accountNumber) - Daniel
+    //private getBills(int accountNumber) - Jesus
 }
 
