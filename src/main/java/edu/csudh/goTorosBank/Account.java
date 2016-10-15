7504ea6c31d7b737579632b1af0cc6717a106871
Brad <bradtaniguchi@gmail.com>
2016-10-15 12:12:32 -0700

    Added transactions to the Account class
    The Account class will now hold data about transactions it has made. Along with this there is a new constructor that represents an Account wit no transactions.

--- src/main/java/edu/csudh/goTorosBank/Account.java
+++ src/main/java/edu/csudh/goTorosBank/Account.java
@@ -1,9 +1,11 @@
 package edu.csudh.goTorosBank;
 
+import java.util.ArrayList;
+
 /**
  * User - an account object. This class represents a User's finances.
  * 
- * @author Jesus Cortez
+ * @author Jesus Cortez, Bradley Taniguchi
  */
 
 public class Account{
@@ -12,15 +14,28 @@ public class Account{
     private String accountType; 
     private int accountBalance;
     private User user;
-    
+    private ArrayList<Transaction> transactions;
+
     /* Account Constructor */
-    public Account(int accountNumber, int accountBalance, User user, String accountType){       
+    public Account(int accountNumber, int accountBalance, User user, String accountType,
+                   ArrayList<Transaction> transactions){
         this.accountNumber = accountNumber;
         this.accountType = accountType;
         this.accountBalance = accountBalance;
         this.user = user;
+        this.transactions = transactions;
     }
-    
+
+    public Account(int accountNumber, int accountBalance, User user, String accountType) {
+        this(accountNumber, accountBalance, user, accountType, null);
+    }
+    /* setter methods */
+    public void addTransaction(Transaction transaction) {
+        this.transactions.add(transaction);
+    }
+    public void addTransactiosn(ArrayList<Transaction> transactions) {
+        this.transactions.addAll(transactions);
+    }
     /*  getter methods    */
     
     public int getAccountNumber(){
