f5c674e065473bc90c45618ad5ee3698929106e5
Brad <bradtaniguchi@gmail.com>
2016-10-19 13:54:53 -0700

    Added warning, and added warning suppress for json

---
+++ src/main/java/edu/csudh/goTorosBank/AccountOverview.java
@@ -0,0 +1,7 @@
+package edu.csudh.goTorosBank;
+
+/**
+ * Created by brad on 10/19/16.
+ */
+public class AccountOverview {
+}
--- src/main/java/edu/csudh/goTorosBank/LoginManager.java
+++ src/main/java/edu/csudh/goTorosBank/LoginManager.java
@@ -13,32 +13,25 @@ import org.json.simple.JSONObject;
  * @see HttpServlet
  */
 public class LoginManager extends HttpServlet {
-    private String userName;
-    private String password;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         getServletContext().log("Init() called");
-        /*TODO: Initialize our 1 temp user ...*/
-        userName = "toro";
-        password = "gotoro";
     }
     @Override
+    @SuppressWarnings("unchecked") //need this to suppress warnings for our json.put
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         getServletContext().log("doPost() called");
         /*TODO: change this code, to do some actual legit checking...*/
-
+        /*need to add a try catch block!!!*/
         if (new DatabaseInterface().validate(
                 request.getParameter("userName"),
                 request.getParameter("password"))
                 )
         {
             response.setContentType("application/json");
-            //Cookie cookie = new Cookie("userName", userName);
-            //cookie.setMaxAge(30*60); //expires in 30 minutes
-            //response.addCookie(cookie);
             HttpSession userSession = request.getSession();
             userSession.setAttribute("userName", request.getParameter("userName"));
             //set other attributes here
@@ -50,35 +43,10 @@ public class LoginManager extends HttpServlet {
             response.setContentType("application/json");
             JSONObject returnJson = new JSONObject();
             returnJson.put("successfulLogin", "false");
-            returnJson.put("message", "Invalid Login fahhhhhhhhg");
-            response.getWriter().write(returnJson.toJSONString());
-        }
-    }
-/*
-        if(request.getParameter("userName").equals(userName) &&
-                request.getParameter("password").equals(password)) {
-
-            response.setContentType("application/json");
-            ///*Cookie cookie = new Cookie("userName", userName);
-            //cookie.setMaxAge(30*60); //expires in 30 minutes
-            //response.addCookie(cookie);
-            HttpSession userSession = request.getSession();
-            userSession.setAttribute("userName", userName);
-            //set other attributes here
-            JSONObject returnJson = new JSONObject();
-            returnJson.put("successfulLogin", "true"); // I get a warning here, disregard..
-            returnJson.put("message", "Valid Login");
-            response.getWriter().write(returnJson.toJSONString());
-        }
-        else {
-            response.setContentType("application/json");
-            JSONObject returnJson = new JSONObject();
-            returnJson.put("successfulLogin", "false");
             returnJson.put("message", "Invalid Login");
             response.getWriter().write(returnJson.toJSONString());
         }
     }
-*/
     @Override
     public void destroy(){
         getServletContext().log("destroy() called");
