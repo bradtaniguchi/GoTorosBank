d9853859f179b80537d5f31c8a782d3538053226
Crosby Lanham <c_a_l310@Yahoo.com>
2016-10-21 13:38:47 -0700

    This is the log in change

--- src/main/java/edu/csudh/goTorosBank/LoginManager.java
+++ src/main/java/edu/csudh/goTorosBank/LoginManager.java
@@ -3,12 +3,15 @@ package edu.csudh.goTorosBank;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.sql.SQLException;
+
 import org.json.simple.JSONObject;
 
 /**
  * Login Manager - manages user logins. Currently only checks a temp account
  * TODO: Add a return cookie, to provide access to other parts of the application
  * TODO: Change the cookie, and utilize SessionID instead.
+ *
  * @author Bradley Taniguchi
  * @see HttpServlet
  */
@@ -19,36 +22,43 @@ public class LoginManager extends HttpServlet {
         super.init(config);
         getServletContext().log("Init() called");
     }
+
     @Override
     @SuppressWarnings("unchecked") //need this to suppress warnings for our json.put
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         getServletContext().log("doPost() called");
         /*TODO: change this code, to do some actual legit checking...*/
-        /*need to add a try catch block!!!*/
-        if (new DatabaseInterface().validate(
-                request.getParameter("userName"),
-                request.getParameter("password"))
-                )
-        {
-            response.setContentType("application/json");
-            HttpSession userSession = request.getSession();
-            userSession.setAttribute("userName", request.getParameter("userName"));
-            //set other attributes here
-            JSONObject returnJson = new JSONObject();
-            returnJson.put("successfulLogin", "true"); // I get a warning here, disregard..
-            returnJson.put("message", "Valid Login");
-            response.getWriter().write(returnJson.toJSONString());
-        } else {
-            response.setContentType("application/json");
-            JSONObject returnJson = new JSONObject();
-            returnJson.put("successfulLogin", "false");
-            returnJson.put("message", "Invalid Login");
-            response.getWriter().write(returnJson.toJSONString());
+        JSONObject returnJson = new JSONObject();
+        try {
+            if (new DatabaseInterface().validate(
+                    request.getParameter("userName"),
+                    request.getParameter("password"))
+                    ) {
+                response.setContentType("application/json");
+                HttpSession userSession = request.getSession();
+                userSession.setAttribute("userName", request.getParameter("userName"));
+                //set other attributes here
+                returnJson.put("successfulLogin", "true"); // I get a warning here, disregard..
+                returnJson.put("message", "Valid Login");
+                response.getWriter().write(returnJson.toJSONString());
+            } else {
+                response.setContentType("application/json");
+                returnJson.put("successfulLogin", "false");
+                returnJson.put("message", "Invalid Login");
+                response.getWriter().write(returnJson.toJSONString());
+            }
+        } catch (SQLException s) {
+            returnJson.put("errormessage","Sorry we have a SQLException");
+            returnJson.put("errormessage2",s);
+        } catch (ClassNotFoundException cl) {
+            returnJson.put("errormessage","Sorry we have a ClassNotFoundProblem");
+            returnJson.put("errormessage2",cl);
         }
     }
+
     @Override
-    public void destroy(){
+    public void destroy() {
         getServletContext().log("destroy() called");
     }
 }
