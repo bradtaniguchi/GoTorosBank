b40c7dea9ae5174d246e0608d2d952f441d85d7c
Brad <bradtaniguchi@gmail.com>
2016-10-21 15:00:19 -0700

    Updated front-end capture of the JSON return

--- src/main/webapp/js/accountOverviewManager.js
+++ src/main/webapp/js/accountOverviewManager.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
                 console.log("successfulQuery: " + response["successfulQuery"]);
                 if(response["successfulQuery"]) {
                     $('#numberOfAccounts').text("Number of Accounts: " + response["accounts"]);
+                    /*test print*/
+                    console.log("test1");
+                    console.log("JSON: " + JSON.stringify(response["userAccounts"]));
+                    console.log("test");
                 }
             },
             error: function(xhr, status, error) {
