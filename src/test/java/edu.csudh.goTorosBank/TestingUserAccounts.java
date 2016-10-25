5aadcbe7eab9a9ee4eefaa6999c59df865f16dec
jeuscortez <jic619@yahoo.com>
2016-10-24 20:07:59 -0700

    Updating Test Class

--- src/test/java/edu.csudh.goTorosBank/TestingUserAccounts.java
+++ src/test/java/edu.csudh.goTorosBank/TestingUserAccounts.java
@@ -16,6 +16,7 @@ public class TestingUserAccounts extends TestCase {
     protected Account TheYodaAccount;
     protected ArrayList<Account> accounts;
     protected ArrayList<Account> yodaAccounts;
+    protected Transaction SampleTransaction;
 
     @Override
     protected void setUp() {
@@ -32,6 +33,8 @@ public class TestingUserAccounts extends TestCase {
         TheYodaAccount = new Account(100, 900000000, Yoda, "LightSabers");
         yodaAccounts.add(TheYodaAccount);
         //Yoda has one account: LightSabers
+        SampleTransaction = new Transaction(TheYodaAccount, 1, 120.00f, "May 18 2010", "Food");
+        TheYodaAccount.addTransaction(SampleTransaction);
     }
 
     public void testId() {
@@ -57,5 +60,37 @@ public class TestingUserAccounts extends TestCase {
         assertEquals(2, Dummy.getUserAccounts().size());
         //attempt to verify that 1 account exists for Yoda
         assertEquals(1, Yoda.getUserAccounts().size());
+    }
+
+    public void testAccountNumber() {
+        assertEquals(900, DummyAccount1.getAccountNumber());
+        assertEquals(310, DummyAccount2.getAccountNumber());
+        assertEquals(100, TheYodaAccount.getAccountNumber());
+    }
+
+    public void testAccountType() {
+        assertEquals("gambling", DummyAccount1.getAccountType());
+        assertEquals("savings", DummyAccount2.getAccountType());
+        assertEquals("LightSabers", TheYodaAccount.getAccountType());
+    }
+
+    public void testAccountBalance() {
+        assertEquals(100000, DummyAccount1.getAccountBalance());
+        assertEquals(2000, DummyAccount2.getAccountBalance());
+        assertEquals(900000000, TheYodaAccount.getAccountBalance());
+    }
+
+    public void testRetrievingUser() {
+        assertEquals(Dummy, DummyAccount1.getUser());
+        assertEquals(Dummy, DummyAccount2.getUser());
+        assertEquals(Yoda, TheYodaAccount.getUser());
+    }
+
+    public void testTransactions() {
+        //checks for list to not be null
+        assertNotNull(TheYodaAccount.getTransactions());
+
+        //checks that the trasnaction is inside the list
+        assertEquals(1, TheYodaAccount.getTransactions().size());
     }
 }
