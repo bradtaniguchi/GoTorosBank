c8d8e3f7ce409a9a0306b8ee88869376ba447597
Brad <bradtaniguchi@gmail.com>
2016-11-03 17:25:38 -0700

    Removed updated servlet annotations

--- src/main/java/edu/csudh/goTorosBank/UploadServlet.java
+++ src/main/java/edu/csudh/goTorosBank/UploadServlet.java
@@ -23,7 +23,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
@@ -32,7 +31,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
-@WebServlet("/UploadServlet")
+
 public class UploadServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config)throws ServletException{
