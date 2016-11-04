40eb3af7165201be0a5833450cfbe8ed10067e50
jeuscortez <jic619@yahoo.com>
2016-11-04 16:42:32 -0700

    Uploading Tess4j and test class

---
+++ src/test/java/edu.csudh.goTorosBank/TestTess4j.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.csudh.goTorosBank;
+
+import java.io.File;
+import java.io.IOException;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Cortez
+ */
+public class TestTess4j extends TestCase{
+    protected Tess4j tess;
+    protected File imageFile;
+    protected File OutputText;
+    
+    @Override
+    public void setUp() throws IOException{
+        tess = new Tess4j(imageFile);
+    }
+    
+    public void testgetText() throws IOException{
+        OutputText = tess.getText();
+    }
+    
+}
