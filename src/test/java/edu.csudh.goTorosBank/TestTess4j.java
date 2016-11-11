64f89c11c670063451d5e0f364358432405ff1fd
Brad <bradtaniguchi@gmail.com>
2016-11-10 19:45:21 -0800

    Merged commit for TestTEss4j.java

--- src/test/java/edu.csudh.goTorosBank/TestTess4j.java
+++ src/test/java/edu.csudh.goTorosBank/TestTess4j.java
@@ -15,26 +15,7 @@ import net.sourceforge.tess4j.TesseractException;
  *
  * @author Cortez
  */
-<<<<<<< Updated upstream
-public class TestTess4j extends TestCase{
-    //protected Tess4j tess;
-    //protected File imageFile = new File ("jdbc:sqlite::resource:ModifiedCheckTemplate.jpg");
-    protected File OutputText;
 
-    /*@Override
-    public void setUp() throws IOException{
-        tess = new Tess4j(imageFile);
-    }*/
-    
-    /*public void testgetText() throws IOException{
-        OutputText = tess.getText();
-    }*/
-    /*to auto pass the test class*/
-    public void testHolder() {
-        assertTrue(true);
-    }
-}
-=======
 public class TestTess4j extends TestCase {
 
     protected Tess4j tess;
@@ -71,4 +52,3 @@ public class TestTess4j extends TestCase {
          
     }
 }
->>>>>>> Stashed changes
