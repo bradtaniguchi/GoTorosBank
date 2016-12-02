3a9bf536e1362c14f911e0b41fd817fdb4611f19
Daniel Hernandez <danielh0193@gmail.com>
2016-12-02 14:13:30 -0800

    Minor text fixes, code cleanup
    Code cleanup on DepositServlet.java and button text fixed on deposit.jsp

--- src/main/java/edu/csudh/goTorosBank/DepositServlet.java
+++ src/main/java/edu/csudh/goTorosBank/DepositServlet.java
@@ -2,7 +2,7 @@ package edu.csudh.goTorosBank;
 
 /**
  *
- * @author Rudy and Juicy J
+ * @author Rudy and Jesus
  */
 import java.io.*;
 
@@ -21,14 +21,15 @@ import org.json.simple.JSONObject;
  * uploads servlets.
  */
 @MultipartConfig
-public class DepositServlet extends HttpServlet {
-
+public class DepositServlet extends HttpServlet
+{
     private static final String SAVE_DIR = "TempUpload"; //where we want to save the files
     private static String DATA_PATH;
     private static boolean readable;
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig config) throws ServletException
+    {
         super.init(config);
         DATA_PATH = getServletContext().getRealPath("");
         DATA_PATH = DATA_PATH + "WEB-INF/classes";
@@ -37,91 +38,126 @@ public class DepositServlet extends HttpServlet {
 
     @Override
     @SuppressWarnings("unchecked")
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         JSONObject returnJson = new JSONObject();
+
         /*check if the user is logged in first*/
         HttpSession session = request.getSession();
         String username = (String) session.getAttribute("username"); //get the user name, and validate they exist
 
-        if (username == null) {
+        if (username == null)
+        {
             returnJson.put("successfulUpload", false);
             returnJson.put("message", "Not Logged in!");
+
             response.getWriter().write(returnJson.toJSONString());
             return;
         }
-        if (!ServletFileUpload.isMultipartContent(request)) {
-            //out.println("Nothing to upload, not multipart content!");
+        if (!ServletFileUpload.isMultipartContent(request))
+        {
             returnJson.put("successfulUpload", false);
             returnJson.put("message", "Nothing to upload, not multipart content!");
-        } else {
+        }
+        else
+        {
             Part fileItem = request.getPart("file"); //get the file part
             String fileName = fileItem.getSubmittedFileName(); //and the name
             String fileType = fileItem.getContentType(); //get type
-            if (fileType.equals("image/png") || fileType.equals("image/jpeg")) {
+
+            if (fileType.equals("image/png") || fileType.equals("image/jpeg"))
+            {
                 // gets absolute path of the web application
                 String appPath = request.getServletContext().getRealPath("");
+
                 // constructs path of the directory to save uploaded file
                 String savePath = appPath + File.separator + SAVE_DIR;
 
-                try {
+                try
+                {
                     File fileSaveDir = new File(savePath);
-                    if (!fileSaveDir.exists()) {
+
+                    if (!fileSaveDir.exists())
+                    {
                         fileSaveDir.mkdir();
                     }
+
                     fileItem.write(savePath + File.separator + fileName);
                     String fullPath = getServletContext().getRealPath(SAVE_DIR + File.separator + fileName);
                     float amount = getCheckAmount(fullPath);
-                    if (amount == -1) {
+
+                    if (amount == -1)
+                    {
                         returnJson.put("message", "Could not Read the Check! Sorry!");
                         returnJson.put("readable", false);
                         returnJson.put("amount", 0);
-                    } else {
+                    }
+                    else
+                    {
+
                         /*Get the account to deposit to...*/
                         DatabaseInterface database = new DatabaseInterface();
                         User user = database.getUser(username);
+
                         /*Get the amount*/
                         int accountID = Integer.parseInt(request.getParameter("account"));
+
                         /*Get the description for the check*/
                         String description = request.getParameter("description");
+
                         /*add a checker to this parser...*/
                         boolean foundAccount = false;
-                        for (Account acc : user.getUserAccounts()) {
-                            if (acc.getAccountNumber() == accountID) {
+
+                        for (Account acc : user.getUserAccounts())
+                        {
+                            if (acc.getAccountNumber() == accountID)
+                            {
                                 returnJson.put("amount", amount);
                                 returnJson.put("readable", readable);
                                 database.deposit(accountID, amount, description);
                                 foundAccount = true;
                             }
                         }
-                        if (foundAccount) {
+                        if (foundAccount)
+                        {
                             returnJson.put("successfulUpload", true); //full transaction
                             returnJson.put("message", "file successfully uploaded: " + fileName + "\n"
                                     + "for amount: " + amount);
                             returnJson.put("readable", true);
-                        } else {
+                        }
+                        else
+                        {
                             returnJson.put("successfulUpload", false);
                             returnJson.put("message", "We didn't find the account with id: " + accountID);
                         }
                     }
 
-                } catch (Exception e) {
+                }
+                catch (Exception e)
+                {
                     returnJson.put("successfulUpload", false);
                     returnJson.put("message", "Error with upload: " + e.getMessage());
                 }
-            } else {
+            }
+            else
+            {
                 returnJson.put("successfulUpload", false);
                 returnJson.put("message", "Error with upload, image type not recognized: " + fileType);
             }
         }
+
         response.getWriter().write(returnJson.toJSONString());
     }
 
-    public float getCheckAmount(String filepath) throws JeusException, IOException, TesseractException, StringIndexOutOfBoundsException {
+    public float getCheckAmount(String filepath) throws JeusException, IOException,
+            TesseractException, StringIndexOutOfBoundsException
+    {
         String result;
         float returnAmount;
         File checkFile = new File(filepath);
+
         Tesseract instance = new Tesseract();
         instance.setDatapath(DATA_PATH); //assign the training data
 
@@ -131,19 +167,19 @@ public class DepositServlet extends HttpServlet {
         int startIndex = result.indexOf('$');
         int resultIndex = result.indexOf('.');
 
-        //try {
-        if (startIndex > 0 && resultIndex > 0) {
+        if (startIndex > 0 && resultIndex > 0)
+        {
             //checks that '$' and '.' exist
-
             String Extract1 = result.substring(startIndex);
-            //substring from '$' to end of string
 
+            //substring from '$' to end of string
             int subString_dollarSignIndex = Extract1.indexOf('$');
             int subString_PeriodIndex = Extract1.indexOf('.');
             int gapSize = subString_PeriodIndex - subString_dollarSignIndex;
-            //the difference between '$' and '.' index
 
-            if (subString_PeriodIndex > 0 && gapSize < 7) {
+            //the difference between '$' and '.' index
+            if (subString_PeriodIndex > 0 && gapSize < 7)
+            {
                 //if the '.' exists in substring and the distance between '$' and '.' is lesser than 7, it is a valid amount
                 //if the gap_size between '$' and '.' is greater then it must not be a valid amount
 
@@ -151,18 +187,19 @@ public class DepositServlet extends HttpServlet {
                 String Extract2 = Extract1.substring(Extract1.indexOf('$') + 2, subString_PeriodIndex);
                 String amount = Extract2.trim();
                 amount = amount + ".00"; //adds two zeros to the end of extracted form.
+
                 returnAmount = Float.parseFloat(amount); //casts the string to a float
+
                 return returnAmount;
-            } else {
+            }
+            else
+            {
                 throw new JeusException("Image has no proper amount or is not a check");
             }
-        } else {
+        }
+        else
+        {
             throw new JeusException("Image has no proper amount or is not a check");
         }
-
-        //} catch (JeusException e) {
-        //    e.printStackTrace();
-        //}
-        //return 0;
     }
 }
