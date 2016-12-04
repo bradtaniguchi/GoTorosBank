63488b461207c0d1cc0c936280c9e33e8f1da0e6
Brad <bradtaniguchi@gmail.com>
2016-12-04 12:37:15 -0800

    Finished withdraw front-end, needs merge of backend changes
    Now the front-end of the withdraw manager supports error handling with modals, shows the picture in an iframe, with instructions on how to 'download' the image and some basic logic to prevent bad requests, IE not selecting an amount

--- src/main/webapp/js/withdrawManager.js
+++ src/main/webapp/js/withdrawManager.js
@@ -1,7 +1,6 @@
 /**
  * Created by brad on 10/24/16.
  */
-
 $(document).ready(function(){
     console.log("withdrawManager.js called");
     function getAccounts() {
@@ -15,7 +14,7 @@ $(document).ready(function(){
                 console.log("successfulQuery: " + response["successfulQuery"]);
                 if(response["successfulQuery"]) {
                     var User = response["userAccounts"];//create a javascript object from the json
-
+                    user = User; //set global variable
                     makeAccounts(User);
                 }
             },
@@ -38,46 +37,98 @@ $(document).ready(function(){
         });
         accountSelector.html(html);
     }
+
+    /**
+     * This function shows a modal with the check inside.
+     * @param url
+     */
+    function returnCheckModal(url){
+        console.log("Trying to show returnCheckModal");
+        var modal = $('#returnModal');
+        var title = "Your Check is Below";
+        modal.find('.modal-header .modal-title').text(title);
+
+        modal.find(".modal-body").html("<p>Right-Click and save this image</p>" + "<br/>" +
+            "<iframe id='iframe' src=" + url + "></iframe>");
+        /*add a button listener, when they click on it, we redirect to main page*/
+        modal.find("btn").off('click').on('click',function() {
+            window.location = "/";
+        });
+        modal.modal('show');
+    }
+
+    /**
+     * This function shows the modal, with the loading gif,
+     * shown before putting up check.
+     */
+    function showLoadingModal(title, message, showImage) {
+        var modal = $('#returnModal');
+
+        modal.find('.modal-header .modal-title').text(title);
+        if (showImage == true) {
+            var html = '<p style="text-align: center">' + message +
+                '<img src="pictures/hex-loader2.gif" alt="Loading Gif" ' +
+                'style="width:304px;height:228px;"></p>';
+            modal.find('.modal-body').html(html)
+        } else {
+            modal.find('.modal-body').text(message);
+        }
+
+        modal.modal('show');
+    }
     /**
      * This account sends the data to the backend via an ajax call.
-     * @param account
-     * @param personGettingPaid
-     * @param amount
+     * @param account - the account ID to remove from
+     * @param personGettingPaid - the person that will get paid
+     * @param amount - the amount to transfer
+     * @param memo - the memo.
      */
     function withdraw(amount, personGettingPaid, account, memo) {
         console.log("amount: " + amount);
         console.log("Person: " + personGettingPaid);
         console.log("accountID: " + account);
         console.log("memo: " + memo);
-        $.ajax({
-            type:'POST',
-            dataType: 'json',
-            url: 'util/WithdrawServlet',
-            data: {
-                amount: amount,
-                personGettingPaid: personGettingPaid,
-                accountID: account,
-                billType: memo
-            },
-            success: function(response) {
-                console.log("message: " + response["message"]);
-                console.log("successfulWithdraw " + response["successfulWithdraw"]);
-                console.log("filename: " + response["filename"]);
-                var filename = response["filename"];
-                url= "/util/WithdrawServlet?filename=" + filename;
-                $('#image-return').html("<iframe id='iframe' src=" + url + "></iframe>");
-
-                /*now change the size of the iframe*/
-                $("#iframe").width(800);
-                $("#iframe").height(600);
-            },
-            error: function(xhr, status, error) {
-                console.log("ERROR with gettingAccountData! See Below for statements");
-                console.log("XHR: " + xhr.responseText);
-                console.log("Status: " + status.responseText);
-                console.log("Error: " + error.responseText);
-            }
-        });
+        /*do a little front-end business logic*/
+        if (personGettingPaid == "" || memo == "") {
+            showLoadingModal("Error!", "You must fill out all forms to proceed!");
+        } else {
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: 'util/WithdrawServlet',
+                data: {
+                    amount: amount,
+                    personGettingPaid: personGettingPaid,
+                    accountID: account,
+                    billType: memo
+                },
+                success: function (response) {
+                    console.log("message: " + response["message"]);
+                    console.log("successfulWithdraw " + response["successfulWithdraw"]);
+                    console.log("filename: " + response["filename"]);
+                    var filename = response["filename"];
+                    url = "/util/WithdrawServlet?filename=" + filename;
+                    if (response["successfulWithdraw"]) {
+                        /*were good so show the check*/
+                        returnCheckModal(url);
+                    } else {
+                        showLoadingModal("ERROR with withdraw!", "There was an error on the server side, we were not able "+
+                        "withdraw " + amount + "from account with ID: " + account + "for these reasons: \n\n" +
+                        response["message"]);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log("ERROR with  withdraw!");
+                    console.log("XHR: " + xhr.responseText);
+                    console.log("Status: " + status.responseText);
+                    console.log("Error: " + error.responseText);
+                    showLoadingModal("ERROR with withdraw!", "XHR: " + xhr.responseText + "\n" +
+                    "Status: " + status.responseText + "\n" + "Error: " + error.responseText);
+                }
+            });
+            /*This is after the ajax call, we wait for it to load...*/
+            showLoadingModal("Loading...", "We are loading your check from the server, please wait", true);
+        }
     }
     $("#submit").off('click')
         .on('click',function() {
@@ -89,8 +140,11 @@ $(document).ready(function(){
             account = account.replace( /[^\d.]/g, '' );/*just get the number*/
 
             var memo = $('#memo').val().trim();
-
-            withdraw(amount, personGettingPaid, account, memo);
+            if (amount == null) {
+                showLoadingModal("Error!", "You must click on a correct amount!");
+            }else {
+                withdraw(amount, personGettingPaid, account, memo);
+            }
     });
 
     getAccounts();
