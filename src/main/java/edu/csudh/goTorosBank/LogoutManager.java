3fd983bcacd94998accd2d71429d99e1be63c33d
Daniel Hernandez <danielh0193@gmail.com>
2016-12-04 04:26:39 -0800

    Code cleanup on LogoutManager.java

--- src/main/java/edu/csudh/goTorosBank/LogoutManager.java
+++ src/main/java/edu/csudh/goTorosBank/LogoutManager.java
@@ -6,38 +6,49 @@ import java.io.IOException;
 import org.json.simple.JSONObject;
 
 /**
- * Logout Manager - removes the user's credentials.
+ * Logout Manager - ends the user's session and logs them out from the website.
  * removes the user's credentials. from the browser, thus removing access until login again.
  * @author Bradley Taniguchi
  * @see HttpServlet
  */
-public class LogoutManager extends HttpServlet {
+public class LogoutManager extends HttpServlet
+{
     @Override
-    public void init(ServletConfig config) throws ServletException {
+    public void init(ServletConfig config) throws ServletException
+    {
         super.init(config);
         getServletContext().log("Init() called");
     }
+
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
         getServletContext().log("doPost() called");
         response.setContentType("application/json");
 
         HttpSession userSession = request.getSession(false);
-        if(userSession != null) {
+
+        if(userSession != null)
+        {
             userSession.invalidate(); //remove user session, and all objects
+
             JSONObject responseJson = new JSONObject();
-            responseJson.put("successfulLogout", "true"); // I get a warning here, disregard..
+
+            responseJson.put("successfulLogout", "true");
             response.getWriter().write(responseJson.toJSONString());
-        } else { //there is no session!
+        }
+        else
+        {
+            //there is no session!
             JSONObject responseJson = new JSONObject();
-            responseJson.put("successfulLogout", "false"); // I get a warning here, disregard..
+
+            responseJson.put("successfulLogout", "false");
             response.getWriter().write(responseJson.toJSONString());
         }
     }
+
     @Override
     public void destroy() {
         getServletContext().log("destroy() called");
     }
-
 }
